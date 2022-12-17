076beb436ace373a767a48a052b35411e260e135
Hudson-Matos <matoshudson07@gmail.com>
2022-12-16 22:30:14 -0300

    salvando os dados na localStorage

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,8 @@ const btn_addNotes = document.querySelector("#btn-new-note");
 const notesContainer = document.querySelector(".notes-container");
 const searchInput = document.querySelector("#search-input");
 
+updatePage();
+
 searchInput.addEventListener("input", filterNotes);
 
 function filterNotes() {
@@ -16,24 +18,29 @@ function filterNotes() {
 
 btn_addNotes.addEventListener('click', createNotes);
 
-function createNotes() {
+function createNotes(obj) {
     const note = document.createElement("div");
     note.classList.add("note-content");
+    if(obj.class) {note.classList.add("fixed");}
 
     const textarea = document.createElement("textarea");
     textarea.placeholder="Digite sua tarefa";
+    textarea.value = obj.description;
 
     const pinIcon = document.createElement("i");
     pinIcon.classList.add("bi");
     pinIcon.classList.add("bi-pin");
     pinIcon.addEventListener("click", ()=>{
         note.classList.toggle("fixed");
+        updateLocalStorage();
     });
 
     const xIcon = document.createElement("i");
     xIcon.classList.add("bi");
     xIcon.classList.add("bi-x-lg");
-    xIcon.addEventListener("click", ()=>{note.remove()})
+    xIcon.addEventListener("click", ()=>{
+        note.remove()
+        updateLocalStorage(); })
 
     const fileIcon = document.createElement("i");
     fileIcon.classList.add("bi");
@@ -52,5 +59,24 @@ function createNotes() {
     notesContainer.appendChild(note);
 
     textarea.focus();
+    textarea.addEventListener("input", updateLocalStorage);
+    updateLocalStorage();
+}
+
+function updateLocalStorage() {
+    const notesList = [];
+    for(var cont of notesContainer.children) {
+        const note = cont.querySelector("textarea");
+        const classe = cont.classList.contains("fixed");
+        notesList.push({description : note.value, class : classe}); }
+    localStorage.setItem("notes", JSON.stringify(notesList));
+}
+
+function updatePage() {
+    const notesList = JSON.parse(localStorage.getItem("notes"));
+    for(var cont of notesList) {
+        createNotes(cont);
+    }
+    
 }
 
